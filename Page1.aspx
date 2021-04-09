18a3ee930e1691c230b5a0d5676ae3147b50871f
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-04-09 21:44:33 +0200

    done

---
+++ Page1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="Partie_7_Ado.net.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Page1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Partie_7_Ado.net
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        static string conString = @"data source=localhost\SQLEXPRESS;database=DBStagiaire;Integrated Security=true;";
+        SqlConnection connection;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(conString);
+            SqlCommand command = new SqlCommand("select * from Stagiaire",connection);
+            connection.Open();
+            GridView1.DataSource = command.ExecuteReader();
+            GridView1.DataBind();
+            connection.Close();
+            
+        }
+    }
+}
